2d3803e192d1082de850a6ce3c0aa043e459b83e
Yaroslav <yaroslav515@mail.ru>
2024-05-16 11:03:16 +0300

    Update settings.py

--- landingpage/landingpage/settings.py
+++ landingpage/landingpage/settings.py
@@ -17,9 +17,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv('../' + str(BASE_DIR))
+os.path.join('../' + str(BASE_DIR))
 
-environment = os.getenv("ENV")
+environment = os.environ["ENV"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
