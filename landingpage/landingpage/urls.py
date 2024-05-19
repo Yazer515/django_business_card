20fe9afce727b407269eb28390c7ea5dacf734c0
Yazer515 <yaroslav515@mail.ru>
2024-05-19 17:19:41 +0300

    Update urls.py

--- landingpage/landingpage/urls.py
+++ landingpage/landingpage/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings # to import static in deployment
+from django.conf.urls.static import static # to import static in deployment
 
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',TemplateView.as_view(template_name="index.html"), name="index.html"),
-]
+] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # to import static in deployment
